3882ca0999d193197e70164e0041dafa9c4bcf5e
yumie-web <zkok.0101.dai@gmail.com>
2023-08-08 23:53:43 +0900

    8/8 header-sp途中まで

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,11 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  $('.header__drawer').on('click',function(){
+    $('.header__drawer').toggleClass('is-open');
+
+    return false;
+  })
+
 
 });
