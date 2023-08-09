d0e79c81e1bd8e1d58e6e72bc9b6206b7dfe1e8a
yumie-web <zkok.0101.dai@gmail.com>
2023-08-10 00:57:18 +0900

    8/9

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,8 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   $('.header__drawer').on('click',function(){
     $('.header__drawer').toggleClass('is-open');
-
+    $('.sp-nav').toggleClass('is-open');
+    $('.header').toggleClass('is-color')
     return false;
   })
 
