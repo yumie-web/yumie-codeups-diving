acc1f430052f0d8730ee4c997b7a71b0441a844a
yumie-web <zkok.0101.dai@gmail.com>
2023-08-14 00:00:17 +0900

    8/13 campaign途中まで

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,23 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  //　ハンバーガー
   $('.js-hamburger').on('click',function(){
     $('.header__drawer').toggleClass('is-open');
     $('.sp-nav').toggleClass('is-open');
     $('.header').toggleClass('is-color')
     return false;
   })
+
+  const swiper = new Swiper('.js-mv-swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade',
+    speed: 2000,
+    autoplay: {
+      delay: 4000,
+    }
+  });
 
 
 });
