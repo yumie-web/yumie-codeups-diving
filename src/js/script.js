fb972e1c3ad30007d5ed7947c214aeae8cd9aecc
yumie-web <zkok.0101.dai@gmail.com>
2023-08-16 00:48:09 +0900

    8/15 inforまで

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-  //　ハンバーガー
+  //ハンバーガー
   $('.js-hamburger').on('click',function(){
     $('.header__drawer').toggleClass('is-open');
     $('.sp-nav').toggleClass('is-open');
