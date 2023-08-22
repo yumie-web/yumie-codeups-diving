55e4bdf98c61d978fe3ced9bb3aefcfa7343ae8a
yumie-web <zkok.0101.dai@gmail.com>
2023-08-22 19:47:41 +0900

    8/22 imgスライドアニメーションまで

--- src/js/script.js
+++ src/js/script.js
@@ -15,20 +15,22 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     loop: true,
     effect: 'fade',
     speed: 2000,
-    // autoplay: {
-    //   delay: 4000,
-    // }
+    autoplay: {
+      delay: 4000,
+    }
   });
 
   // campaign
   var swiper = new Swiper('.js-campaign-swiper', {
-    // Optional parameters
     loop: true,
-    slidesPerView: "auto",
+    slidesPerView: 1.26,
     spaceBetween: 24,
-    allowTouchMove: true,
     breakpoints: {
       768: { //768px以上
+        slidesPerView: 3.3,
+        spaceBetween: 30,
+      },
+      1024: { //1024px以上
         slidesPerView: 3.5,
         spaceBetween: 40,
       },
@@ -40,5 +42,33 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     },
   });
 
+  //要素の取得とスピードの設定
+  var box = $('.js-color'),
+  speed = 700;
+
+  //.colorboxの付いた全ての要素に対して下記の処理を行う
+  box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+      if(counter == 0){
+        $(this).delay(200).animate({'width':'100%'},speed,function(){
+          image.css('opacity','1');
+          $(this).css({'left':'0' , 'right':'auto'});
+          $(this).animate({'width':'0%'},speed);
+        })
+        counter = 1;
+      }
+    });
+  });
+
+
 
 });
+
